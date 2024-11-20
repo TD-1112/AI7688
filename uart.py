aa0f63761d9da9da6e44ec732c8ba242f4b02f62
TD-1112 <142862400+TD-1112@users.noreply.github.com>
2024-11-20 10:38:45 +0700

    Update uart.py

--- uart.py
+++ uart.py
@@ -1,9 +1,9 @@
 import serial
 import time
 
-# Cấu hình UART
+
 ser = serial.Serial(
-    port='/dev/ttyUSB0',       # Thay COM3 bằng cổng UART của bạn (Linux có thể là /dev/ttyUSB0)
+    port='/dev/ttyUSB0',       
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -11,16 +11,15 @@ ser = serial.Serial(
     timeout=1
 )
 
-# Gửi dữ liệu
+
 while True:
     while True:
         message = "hello"
-        ser.write(message.encode('utf-8'))  # Gửi chuỗi
+        ser.write(message.encode('utf-8'))  
         print(f"Sent: {message}")
 
-        # Nhận lại dữ liệu từ AI7688
         response = ser.readline().decode('utf-8').strip()
         if response:
             print(f"Received: {response}")
 
-        time.sleep(1)  # Chờ 1 giây trước khi gửi lại
+        time.sleep(1) 
